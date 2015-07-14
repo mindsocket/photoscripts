ece0d5b2bf325cc05ae9f7253b88db9b381ba116
Roger Barnes <rbarnes@atlassian.com>
2015-07-14 14:42:54 +1000

    something something

--- averagehue.py
+++ averagehue.py
@@ -4,6 +4,12 @@ import math
 from PIL import Image
 import colorsys
 import numpy as np
+from itertools import izip_longest
+
+def grouper(n, iterable, fillvalue=None):
+    "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue=fillvalue, *args)
 
 def hue(r,g,b):
     ''' Ganked from colorsys.rgb_to_hsv'''
@@ -30,26 +36,39 @@ def circular_mean(angles):
     See: http://en.wikipedia.org/wiki/Mean_of_circular_quantities'''
     n = len(angles)
     mean = math.atan2(sum([math.sin(a) for a in angles]) / float(n), sum([math.cos(a) for a in angles]) / float(n))
-    print mean
+    #print mean
     return mean
+
+circular_average = lambda x, y: (x + y) / 2 if abs(x-y) < 0.5 else ((x + y - 1) / 2) % 1.0 
 
 def hue_array(image):
 	# get pixels
-    arr = np.array(image.convert('RGBA'))
+    arr = np.array(image.convert('RGBA').resize((200,200)))
     r, g, b, a = np.rollaxis(arr, axis=-1)
     # get hue of each
     h = rgb_to_hue(r, g, b)
 #    print h.shape
 #    print h[0]
 	# convert from 0..1 scale to 0..2pi (radians)
-    h *= 2 * math.pi
+    #h *= 2 * math.pi
 #    print h[0]
     return h.flatten()
 
+def reducer(l):
+    pairs = l
+    while len(pairs)>1:
+        pairs = [circular_average(pairs[i*2],pairs[i*2+1]) for i in xrange(len(pairs)/2)]
+    return pairs[0]
+
+
 if __name__ == '__main__':
-    print "Getting file list"
+    #print "Getting file list"
     files = sys.argv[1:]
 
-    result = [(circular_mean(hue_array(Image.open(file))), file) for file in files]
-
-    print sorted(result)
+    #result = [(circular_mean(hue_array(Image.open(file))), file) for file in files]
+    #print int((result[0][0]+math.pi)*50)
+    #result = [(reduce(circular_average,sorted(hue_array(Image.open(file)))), file) for file in files]
+    sorted_hue_array = lambda file: sorted(hue_array(Image.open(file)))
+    result = [(reducer(sorted_hue_array(file)), file) for file in files]
+    print  "%03d" % int(result[0][0]*500)
+    #print sorted(result)
